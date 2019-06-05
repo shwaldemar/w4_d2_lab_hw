b77f090dcf7fcf927f353ef1d73733959f3eb7da
Simon <shwaldemar@gmail.com>
2019-06-05 13:11:42 +0100

    tested & fixed

--- controller.rb
+++ controller.rb
@@ -1,3 +1,6 @@
+#test game browser path http://localhost:4567/game/rock/paper
+
+
 require("pry-byebug")
 require("sinatra")
 require("sinatra/contrib/all")
@@ -8,9 +11,17 @@ require_relative("./models/game")
 also_reload("./models")
 
 get "/game/:user_choice/:computer_choice" do
-  user_choice = params["user_choice"]
-  computer_choice = params["computer_choice"]
-  game = game.new(user_choice, computer_choice)
+  @user_choice = params["user_choice"]
+  @computer_choice = params["computer_choice"]
+  game = Game.new(@user_choice, @computer_choice)
   @game_result = game.game()
   erb(:result)
+end
+
+get "/welcome" do
+  erb(:welcome)
+end
+
+get "/" do
+  redirect "/game/rock/paper"
 end
--- models/game.rb
+++ models/game.rb
@@ -1,5 +1,5 @@
 class Game
-
+attr_reader :user_choice, :computer_choice
   def initialize(user_choice, computer_choice)
     @user_choice = user_choice
     @computer_choice = computer_choice
@@ -8,21 +8,21 @@ class Game
 def game()
 
 #rock v scissors
-  if @user_choice == "r" && @computer_choice == "scissors"
+  if @user_choice == "rock" && @computer_choice == "scissors"
     return "user wins - rock beats scissors."
-  elsif @user_choice == "s" && @computer_choice == "rock"
+  elsif @user_choice == "scissors" && @computer_choice == "rock"
     return "computer wins - rock beats scissors."
 
 # rock v paper
-elsif @user_choice == "p" && @computer_choice == "rock"
+elsif @user_choice == "paper" && @computer_choice == "rock"
     return "user wins - paper beats rock."
-  elsif @user_choice == "r" && @computer_choice == "paper"
+  elsif @user_choice == "rock" && @computer_choice == "paper"
     return "computer wins - paper beats rock."
 
 # scissors v paper
-elsif @user_choice == "s" && @computer_choice == "paper"
+elsif @user_choice == "scissors" && @computer_choice == "paper"
     return "user wins - scissors beat paper"
-  elsif @user_choice == "p" && @computer_choice == "scissors"
+  elsif @user_choice == "paper" && @computer_choice == "scissors"
     return "computer wins - scissors beat paper"
 
   else
